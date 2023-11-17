d96b0dcffd9a2f780253345ca664af34bf8492bb
Own-DevTy <own.devty@gmail.com>
2023-11-18 01:43:01 +0900

    bug fix component chart

--- components/chart/chart.jsx
+++ components/chart/chart.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import {
-  Chart as ChartJS,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  CategoryScale,
-  Filler,
-  Tooltip,
-  Legend,
-  Title,
+    Chart as ChartJS,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    CategoryScale,
+    Filler,
+    Tooltip,
+    Legend,
+    Title,
 } from 'chart.js';
-import { Radar } from 'react-chartjs-2';
+import {Radar} from 'react-chartjs-2';
 // import { setLabels } from 'react-chartjs-2';
 
 ChartJS.register(
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  CategoryScale,
-  Filler,
-  Tooltip,
-  Legend,
-  Title
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    CategoryScale,
+    Filler,
+    Tooltip,
+    Legend,
+    Title
 );
 
 // 차트의 옵션값
@@ -40,25 +40,25 @@ export function setlabels(bool) {
     // 타율, 출루율, 장타율, 출루율+장타율, 홈런
     const labels1 = [
         '타율', '출루율', '장타율',
-        ['출루율','+장타율'], '홈런'
+        ['출루율', '+장타율'], '홈런'
     ];
 
     // 삼진 아웃 수, 평균 자책점, 볼넷 수, 출루허용률,
     // 0이닝당 평균 삼진 수
     const labels2 = [
-        ['삼진','아웃 수'], ['평균','자책점'], ['볼넷',' 수'],
-        ['출루','허용률'], 'K/9'
+        ['삼진', '아웃 수'], ['평균', '자책점'], ['볼넷', ' 수'],
+        ['출루', '허용률'], 'K/9'
     ];
     // true일 경우 labels1 아닐 경우 labels2
     return bool ? labels1 : labels2;
 }
 
 /**
-* 선수 A, B의 이름과 스탯을 입력받아 차트를 생성
-* @param {string} name1 Player A
-* @param {string} name2 Player B
-* @returns PlayerA와 PlayerB의 비교 차트 생성
-*/
+ * 선수 A, B의 이름과 스탯을 입력받아 차트를 생성
+ * @param {string} name1 Player A
+ * @param {string} name2 Player B
+ * @returns React.JSX.Element PlayerB의 비교 차트 생성
+ */
 export default function Chart(
     nameA, x_data1, x_data2, x_data3, x_data4, x_data5,
     nameB, y_data1, y_data2, y_data3, y_data4, y_data5,
@@ -80,7 +80,7 @@ export default function Chart(
                 borderColor: 'rgba(223, 53, 18, 0.4)',
                 borderWidth: 2,
                 pointBorderColor: 'rgba(223, 53, 18, 0.5)',
-                
+
             },
             {
                 label: nameB,
@@ -98,17 +98,15 @@ export default function Chart(
             }
         ],
     };
-
+    const handleResize = () => {
+    }
     return (
-        <div>
-            <div>
-                <Radar
-                title= "Radar Chart"
-                description="Stats"
-                data={data}
-                option={option}
-                />
-            </div>
-        </div>
+        <Radar
+            title="Radar Chart"
+            description="Stats"
+            data={data}
+            option={option}
+            height={"100%"}
+            width={"100%"}/>
     )
 }
--- pages/compare/index.jsx
+++ pages/compare/index.jsx
@@ -57,11 +57,11 @@ function App() {
                         <div className={styles.center_top}>             MLB 로고 삽입               </div>
                         <br/><br/>
                         <div className={styles.center_bottom}>
-                            {Chart(
+                            {RChart(
                                 "Name1", 1, 2, 3, 4, 5,
                                 "Name2", 3, 3, 2, 5, 6, true
                             )}
-                            {Chart(
+                            {RChart(
                                 "C", 1, 2, 3, 4, 5,
                                 "D", 6, 3, 2, 3, 6, false
                             )}
