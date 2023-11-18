9f1850d10aea192a944fb26df102c02707e984ca
Own-DevTy <own.devty@gmail.com>
2023-11-19 06:06:44 +0900

    modify backend 링크를 환경에 따라 바뀌도록 수정

--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -30,11 +30,9 @@ export default function Search({id, hitting_json, pitching_json}) {
 
 export async function getServerSideProps(context) {
     const id = context.params.id;
-    // const hitting_res = await fetch(`http://127.0.0.1:8000/api/v1/search/hitting/${id}`);
-    // const pitching_res = await fetch(`http://127.0.0.1:8000/api/v1/search/pitching/${id}`);
+    const hitting_res = await fetch(`${process.env.api}/search/hitting/${id}`);
+    const pitching_res = await fetch(`${process.env.api}/search/pitching/${id}`);
 
-    // const [hitting_json, pitching_json] = await Promise.all([hitting_res.json(), pitching_res.json()])
-    const hitting_json = {}
-    const pitching_json = {}
+    const [hitting_json, pitching_json] = await Promise.all([hitting_res.json(), pitching_res.json()])
     return {props: {id, hitting_json, pitching_json}}
 }
