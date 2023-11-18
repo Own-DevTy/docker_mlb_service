41ba6e093f595afbe349b0055a2919e3e0035a63
Own-DevTy <own.devty@gmail.com>
2023-11-19 06:22:33 +0900

    remove child repo

--- back/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import BaseOut
-from .player import Hitting, HittingCreate, HittingInDB, HittingUpdate, \
-    Pitching, PitchingCreate, PitchingInDB, PitchingUpdate
-from .team import Team, TeamCreate, TeamInDB, TeamUpdate
