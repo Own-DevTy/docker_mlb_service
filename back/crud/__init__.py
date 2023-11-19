f2900206f157023947d697e43ab48d6d9c2bd80b
Own-DevTy <own.devty@gmail.com>
2023-11-19 09:34:23 +0900

    add prestart.sh

--- back/crud/__init__.py
+++
@@ -1,3 +0,0 @@
-from .crud_team import team
-from .crud_hitting import hitting
-from .crud_pitching import pitching
--- back/models/team.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String
-
-from db.base_class import Base
-
-
-class Team(Base):
-    league_name = Column(String(50), nullable=False)
-    link = Column(String(2048), nullable=False)
