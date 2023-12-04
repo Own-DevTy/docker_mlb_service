09d89dd191f5873b63984f37d41ffa3bc9097b20
Own-DevTy <own.devty@gmail.com>
2023-12-04 22:06:37 +0900

    modify fetch path

--- components/table/fetchData.jsx
+++ components/table/fetchData.jsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState, useCallback, Fragment} from 'react';
+import React, { useEffect, useState, useCallback, Fragment } from 'react';
 
 const fetchDataFromAPI = async (id) => {
     try {
-        const response = await fetch(`http://127.0.0.1:8000/api/v1/search/hitting/${id}`);
+        const response = await fetch(`${process.env.api}/search/hitting/${id}`);
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
@@ -14,10 +14,8 @@ const fetchDataFromAPI = async (id) => {
     }
 };
 const initial_state = {
-    hitting: [
-        {id: "", name: ""}
-    ]
-}
+    hitting: [{ id: '', name: '' }],
+};
 const YourComponent = () => {
     const [data, setData] = useState(initial_state);
 
@@ -27,27 +25,25 @@ const YourComponent = () => {
     }, []);
 
     useEffect(() => {
-        fetchData(108);         //팀번호 입력 넣으셈
+        fetchData(108); //팀번호 입력 넣으셈
     }, []);
 
     if (!data) {
         return <div>Loading...</div>;
     }
 
-   
-    const playerId = 500871;    //선수번호 입력 하셈
-    const playerInfo = data.hitting.find(player => player.id === playerId);
+    const playerId = 500871; //선수번호 입력 하셈
+    const playerInfo = data.hitting.find((player) => player.id === playerId);
 
     return (
         <Fragment>
-            {playerInfo && (            //세부정보 추가하면 됨
+            {playerInfo && ( //세부정보 추가하면 됨
                 <div>
                     <p>아이디 : {playerInfo.id}</p>
                     <p>이름 : {playerInfo.name}</p>
-                    <p>나이 : { playerInfo.age}</p>
-                    <p>키 : { playerInfo.height}</p>
-                    <p>몸무게 : { playerInfo.weight}</p>
-                  
+                    <p>나이 : {playerInfo.age}</p>
+                    <p>키 : {playerInfo.height}</p>
+                    <p>몸무게 : {playerInfo.weight}</p>
                 </div>
             )}
         </Fragment>
