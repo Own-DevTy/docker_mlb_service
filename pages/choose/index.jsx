ccabe56191a194e56090177123ccf14d0ed73866
Own-DevTy <own.devty@gmail.com>
2023-12-08 18:29:04 +0900

    modify choose > css, index, paginationData

--- pages/choose/index.jsx
+++ pages/choose/index.jsx
@@ -13,6 +13,7 @@ import {
 import { usePagination } from '@table-library/react-table-library/pagination';
 import getPlayerInfo from '@/pages/choose/paginationData.jsx';
 import { useTheme } from '@table-library/react-table-library/theme';
+import { Fragment } from 'react';
 
 const Component = () => {
     const LIMIT = 10;
@@ -45,7 +46,7 @@ const Component = () => {
     });
 
     const fetchData = React.useCallback((offset, limit) => {
-        getPlayerInfo('hitting', offset, limit)
+        getPlayerInfo('pitching', offset, limit)
             .then((playerInfo) => {
                 setData({
                     nodes: playerInfo.nodes,
@@ -85,117 +86,113 @@ const Component = () => {
     }
 
     return (
-        <>
-            <div className={styles.main_box}>
-                <div className={styles.intro_player}>
-                    {' '}
-                    <p>인트로 선수</p>{' '}
-                </div>
-                <div className={styles.player_table}>
-                    <Table data={data} pagination={pagination} theme={theme}>
-                        {(tableList) => (
-                            <>
-                                <Header>
-                                    <HeaderRow>
-                                        <HeaderCell>팀 이름</HeaderCell>
-                                        <HeaderCell>선수 이름</HeaderCell>
-                                    </HeaderRow>
-                                </Header>
+        <div className={styles.main_box}>
+            <div className={styles.intro_player}>
+                <p>인트로 선수</p>{' '}
+            </div>
+            <div className={styles.player_table}>
+                <Table data={data} pagination={pagination} theme={theme}>
+                    {(tableList) => (
+                        <>
+                            <Header>
+                                <HeaderRow>
+                                    <HeaderCell>팀 이름</HeaderCell>
+                                    <HeaderCell>선수 이름</HeaderCell>
+                                </HeaderRow>
+                            </Header>
 
-                                <Body>
-                                    {tableList.map((item, index) => (
-                                        <Row key={index}>
-                                            <Cell
-                                                onClick={() =>
-                                                    handleCellClick(item, index)
-                                                }
-                                            >
-                                                {item.team_name}
-                                            </Cell>
-                                            <Cell
-                                                onClick={() =>
-                                                    handleCellClick(item, index)
-                                                }
-                                            >
-                                                {item.name}
-                                            </Cell>
-                                        </Row>
-                                    ))}
-                                </Body>
-                            </>
-                        )}
-                    </Table>
-                    <div className={styles.list}></div>
-                    <div className={styles.compare_btn}>
-                        <a href="./compare">
-                            <button className={styles.Button1}>
-                                스탯 비교 시작
-                            </button>{' '}
-                        </a>
-                    </div>
+                            <Body>
+                                {tableList.map((item, index) => (
+                                    <Row key={index}>
+                                        <Cell
+                                            onClick={() =>
+                                                handleCellClick(item, index)
+                                            }
+                                        >
+                                            {item.team_name}
+                                        </Cell>
+                                        <Cell
+                                            onClick={() =>
+                                                handleCellClick(item, index)
+                                            }
+                                        >
+                                            {item.name}
+                                        </Cell>
+                                    </Row>
+                                ))}
+                            </Body>
+                        </>
+                    )}
+                </Table>
+                <div className={styles.list}></div>
+                <div className={styles.compare_btn}>
+                    {/*<a href="./compare">*/}
+                    {/*    <button className={styles.Button1}>*/}
+                    {/*        스탯 비교 시작*/}
+                    {/*    </button>{' '}*/}
+                    {/*</a>*/}
+                    {data.pageInfo && (
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <span>
+                                페이지 : {pagination.state.page + 1} of{' '}
+                                {data.pageInfo.totalPages}{' '}
+                                <button
+                                    type="button"
+                                    disabled={pagination.state.page === 0}
+                                    onClick={() => pagination.fns.onSetPage(0)}
+                                >
+                                    {'|<'}
+                                </button>
+                                <button
+                                    type="button"
+                                    disabled={pagination.state.page === 0}
+                                    onClick={() =>
+                                        pagination.fns.onSetPage(
+                                            pagination.state.page - 1
+                                        )
+                                    }
+                                >
+                                    {'<'}
+                                </button>
+                                <button
+                                    type="button"
+                                    disabled={
+                                        pagination.state.page + 1 ===
+                                        data.pageInfo.totalPages
+                                    }
+                                    onClick={() =>
+                                        pagination.fns.onSetPage(
+                                            pagination.state.page + 1
+                                        )
+                                    }
+                                >
+                                    {'>'}
+                                </button>
+                                <button
+                                    type="button"
+                                    disabled={
+                                        pagination.state.page + 1 ===
+                                        data.pageInfo.totalPages
+                                    }
+                                    onClick={() =>
+                                        pagination.fns.onSetPage(
+                                            data.pageInfo.totalPages - 1
+                                        )
+                                    }
+                                >
+                                    {'>|'}
+                                </button>
+                            </span>
+                        </div>
+                    )}
                 </div>
             </div>
-            {data.pageInfo && (
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                    }}
-                >
-                    <span></span>
-                    <span>
-                        페이지 : {pagination.state.page + 1} of{' '}
-                        {data.pageInfo.totalPages}{' '}
-                        <button
-                            type="button"
-                            disabled={pagination.state.page === 0}
-                            onClick={() => pagination.fns.onSetPage(0)}
-                        >
-                            {'|<'}
-                        </button>
-                        <button
-                            type="button"
-                            disabled={pagination.state.page === 0}
-                            onClick={() =>
-                                pagination.fns.onSetPage(
-                                    pagination.state.page - 1
-                                )
-                            }
-                        >
-                            {'<'}
-                        </button>
-                        <button
-                            type="button"
-                            disabled={
-                                pagination.state.page + 1 ===
-                                data.pageInfo.totalPages
-                            }
-                            onClick={() =>
-                                pagination.fns.onSetPage(
-                                    pagination.state.page + 1
-                                )
-                            }
-                        >
-                            {'>'}
-                        </button>
-                        <button
-                            type="button"
-                            disabled={
-                                pagination.state.page + 1 ===
-                                data.pageInfo.totalPages
-                            }
-                            onClick={() =>
-                                pagination.fns.onSetPage(
-                                    data.pageInfo.totalPages - 1
-                                )
-                            }
-                        >
-                            {'>|'}
-                        </button>
-                    </span>
-                </div>
-            )}
-        </>
+        </div>
     );
 };
 
