ad08df78f80da08e4781a91134893d3315049eff
Own-DevTy <own.devty@gmail.com>
2023-12-10 00:21:59 +0900

    merged paginationData

--- pages/choose/index.jsx
+++ pages/choose/index.jsx
@@ -10,12 +10,49 @@ import {
     Cell,
 } from '@table-library/react-table-library/table';
 import { usePagination } from '@table-library/react-table-library/pagination';
-import getPlayerInfo from '@/pages/choose/paginationData.jsx';
 import { useTheme } from '@table-library/react-table-library/theme';
 import { Fragment, useCallback, useEffect, useState } from 'react';
 import PlayerTable from '@/components/table/PlayerTableForChoose';
 import { BiBaseball } from 'react-icons/bi';
 import { FaBaseballBatBall } from 'react-icons/fa6';
+
+function getPlayerInfo(position, offset, limit) {
+    return fetch(
+        `${process.env.api}/all/${position}?skip=${offset}&limit=${limit}`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            const playerInfo = data[position].map((player) => {
+                if (position === 'pitching') {
+                    return {
+                        team_name: player.team_name,
+                        name: player.name,
+                        strikeOuts: player.strikeOuts,
+                        era: player.era,
+                        baseOnBalls: player.baseOnBalls,
+                        whip: player.whip,
+                        strikeoutsPer9Inn: player.strikeoutsPer9Inn,
+                        id: player.id,
+                    };
+                } else if (position === 'hitting') {
+                    return {
+                        team_name: player.team_name,
+                        name: player.name,
+                        avg: player.avg,
+                        obp: player.obp,
+                        slg: player.slg,
+                        ops: player.ops,
+                        homeRuns: player.homeRuns,
+                        id: player.id,
+                    };
+                }
+            });
+
+            return { nodes: playerInfo, size: Math.floor(data.size) };
+        });
+}
 
 const Component = ({ pid, position, player }) => {
     const LIMIT = 10;
